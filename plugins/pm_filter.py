8e92d6da6f1ee302a0481ae297ccade1a09569e5
vikku65569 <vikku6556956@gmail.com>
2025-05-29 23:02:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2674,7 +2674,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
             
             
         else:
-            await reply_msg.edit_text("⚠️ WARNING DON'T SHARE LINKS <br> REQUEST MOVIES @MOTUMOVIES.")
+            await reply_msg.edit_text("⚠️ WARNING DON'T SHARE LINKS <br> REQUEST MOVIES @khushibots.")
             return
     else:
         message = msg.message.reply_to_message  # msg will be callback query
@@ -2864,7 +2864,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
         logger.exception(e)
         reqst_gle = mv_rqst.replace(" ", "+")
         button = [[
-            InlineKeyboardButton("MSG HERE", url=f"https://t.me/motumovies")
+            InlineKeyboardButton("MSG HERE", url=f"https://t.me/khushibots")
         ]]
         if NO_RESULTS_MSG:
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
@@ -2876,7 +2876,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
     if not movies:
         reqst_gle = mv_rqst.replace(" ", "+")
         button = [[
-            InlineKeyboardButton("MSG HERE", url=f"https://t.me/motumovies")
+            InlineKeyboardButton("MSG HERE", url=f"https://t.me/khushibots")
         ]]
         if NO_RESULTS_MSG:
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
@@ -2902,7 +2902,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
                 break
         reqst_gle = mv_rqst.replace(" ", "+")
         button = [[
-            InlineKeyboardButton("MSG HERE", url=f"https://t.me/motumovies{reqst_gle}")
+            InlineKeyboardButton("MSG HERE", url=f"https://t.me/khushibots{reqst_gle}")
         ]]
         if NO_RESULTS_MSG:
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
