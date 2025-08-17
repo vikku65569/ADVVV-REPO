146c912d51ae553d46ba38cb708d2d7eb707e63b
vikku65569 <vikku6556956@gmail.com>
2025-08-17 12:56:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -86,8 +86,8 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/AARLINK')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/AARLINK')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/AARLINKS')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/AARLINKS')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'khushibots') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/wrrohit02')
 
@@ -187,6 +187,7 @@ else:
     OTHER_DB_URI = O_DB_URI       # This Db Is For Other Data Store
     FILE_DB_URI = F_DB_URI        # This Db Is For File Data Store
     SEC_FILE_DB_URI = S_DB_URI    # This Db is for File Data Store When First Db Is Going To Be Full.
+
 
 
 
